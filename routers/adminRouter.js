28f0144a03aa53b08f20278a70901fab4aab1464
gajendra-github <gajendra.github@gmail.com>
2023-03-04 11:38:01 -0500

    update

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
-
-
 const adminRouter = express.Router();
 
 const uri = process.env.MONGO_URI;
 
-const dbName = 'globomantics';
-
-
-
+const dbName = 'gtmc-app';
 
 adminRouter.route('/').get((req, res) => {
-
-
-
     (async function mongo() {
         try {
             console.log('This is admin router');
             client = await MongoClient.connect(uri);
             console.log('Connected to mongo DB');
-            const db = client.db(dbName);
-            const response = await db.collection('sessions').insertMany({ "id": "1" });
-            res.json(response);
+
 
         } catch (error) {
             console.log(error);
