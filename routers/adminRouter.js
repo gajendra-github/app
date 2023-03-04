7773feea951a9006dfd6f819188061ffdd17bf8b
gajendra-github <gajendra.github@gmail.com>
2023-03-04 09:56:19 -0500

    update

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -9,22 +9,25 @@ const uri = process.env.MONGO_URI;
 
 const dbName = 'globomantics2';
 
-adminRouter.route('/').get((req, res) => {
+console.log('This is admin router');
 
-    (async function mongo() {
-        let client;
-        try {
-            client = await MongoClient.connect(uri);
-            console.log('Connected to mongo DB');
 
-            const db = client.db(dbName);
-            const response = await db.collection('sessions').insertMany({ "id": "1" });
-            res.json(response);
-        } catch (error) {
-            console.log(error);
-        }
-    }());
-})
+// adminRouter.route('/').get((req, res) => {
+
+//     (async function mongo() {
+//         let client;
+//         try {
+//             client = await MongoClient.connect(uri);
+//             console.log('Connected to mongo DB');
+
+//             const db = client.db(dbName);
+//             const response = await db.collection('sessions').insertMany({ "id": "1" });
+//             res.json(response);
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     }());
+// })
 
 
 module.exports = adminRouter;
